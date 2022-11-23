5b43098e521f8dd2c0b2391ca2bd760c936ad143
Yusanagi <indexyusanagi@gmail.com>
2022-11-22 23:00:55 -0800

    Added new helper functions + some basic functionality

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/* 
+*  For Checkpoint 2, need to have test plan outline + description of tests
+*  Need to have data structure to represent tag array of cache 
+*  Need to derive index and tag fields from address
+*  Demonstrate functions to implement replacement algorithm
+*/
+
 int main(int argc, char *argv[]) {
     char *filename;
-    traceValues trace;
 
     // Need to have filename and mode select
     if(argc != 3) {
@@ -17,7 +23,7 @@ int main(int argc, char *argv[]) {
                 
                 case 's':
                     printf("Silent Mode Enabled...\n");
-                    silentModeGbl = true;
+                    silentMode = true;
                     break;
                 case 'n':
                     printf("Nornal Mode Enabled...\n");
@@ -33,13 +39,266 @@ int main(int argc, char *argv[]) {
     }
 
     trace = traceParser(filename);
+    resetCache();
+    for(int i = 0; i < trace.size; i++) {
+        switch(trace.command[i]) {
+            case(READREQ):
+                read(trace.address[i]);
+                break;
+            case(WRITEREQ):
+                break;
+            case(READREQI):
+                break;
+            case(SNOOPINVALD):
+                break;
+            case(SNOOPREAD):
+                break;
+            case(SNOOPWRITE):
+                break;
+            case(SNOOPRWIN):
+                break;
+            case(CLR):
+                break;
+            case(PRINT):
+                printValid(trace.address[i]);
+                break;
+            default:
+                exit(-1);
+        }
+        output.cacheRatio = output.cacheHits / (float)(output.cacheReads + output.cacheWrites);
+        printOutput();
+    }
 
     return 0;
 }
 
-traceValues traceParser(char *filename) {
+void read(unsigned int address) {
+    // dissect address into tag and index
+    // check to see if hit
+    // if miss, check to see if any line is invalid
+    // if line invalid, write to line, set MESI
+    // if no invalid, evict line and set MESI
+    Derived values;
+    int way;
+    output.cacheReads++;
+
+    values = addressParser(address);
+    printf("\nTag === 0x%x\n", values.tag);
+    way = findHit(values);
+    
+    if (way != -1) {
+        // Hit
+        output.cacheHits++;
+        switch (dCache[values.index].lines[way].MESI) {
+            case 'M':
+                updateLRU(values.index, way);
+                messageToCache(SENDLINE, address);
+                break;
+            case 'E':
+                dCache[values.index].lines[way].MESI = 'S';
+                updateLRU(values.index, way);
+                messageToCache(SENDLINE, address);
+                break;
+            case 'S':
+                updateLRU(values.index, way);
+                messageToCache(SENDLINE, address);
+                break;
+            case 'I':
+                dCache[values.index].lines[way].MESI = 'S';
+                updateLRU(values.index, way);
+                messageToCache(SENDLINE, address);
+                break;
+            default:
+                exit(-1);
+        }
+    }
+    else {
+        output.cacheMiss++;
+        way = findEmpty(values.index);
+        if (way != -1) {
+            // Miss (Empty)
+            dCache[values.index].lines[way].MESI = 'E';
+            dCache[values.index].lines[way].tag = values.tag;
+            updateLRU(values.index, way);
+            messageToCache(SENDLINE, address);
+        }
+        else {
+            // Miss (Needs Evict)
+            way = getLRU(values.index);
+            dCache[values.index].lines[way].MESI = 'E';
+            dCache[values.index].lines[way].tag = values.tag;
+            updateLRU(values.index, way);
+            messageToCache(SENDLINE, address);
+        }
+    }
+
+}
+
+void write(unsigned int address) {
+    // dissect address into tag and index
+    // check to see if hit
+    // if miss, check to see if any line is invalid
+    // if line invalid, write to line, set MESI
+    // if no invalid, evict line and set MESI
+}
+
+void updateLRU(unsigned int setIndex, int way) {
+    unsigned int PLRU = dCache[setIndex].PLRU;
+
+    switch(way) {
+        case 0:
+            PLRU = PLRU & 0x74;
+            break;
+        case 1:
+            PLRU = (PLRU & 0x74) | 0x8;
+            break;
+        case 2:
+            PLRU = (PLRU & 0x6C) | 0x2;
+            break;
+        case 3:
+            PLRU = (PLRU & 0x6C) | 0x12;
+            break;
+        case 4:
+            PLRU = (PLRU & 0x6A) | 0x1;
+            break;
+        case 5:
+            PLRU = (PLRU & 0x6A) | 0x21;
+            break;
+        case 6:
+            PLRU = (PLRU & 0x3A) | 0x5;
+            break;
+        case 7:
+            PLRU = (PLRU & 0x3A) | 0x45;
+            break;
+    }
+    dCache[setIndex].PLRU = PLRU;
+}
+
+int getLRU(unsigned int setIndex) {
+    int way;
+    unsigned int PLRU = dCache[setIndex].PLRU;
+
+    if( (PLRU & 0x1) == 0 ) {
+        if( ((PLRU >> 2) & 0x1) == 0 ) {
+            if( ((PLRU >> 3) & 0x1) == 0 ) {
+                way = 1;
+            }
+            else {
+                way = 0;
+            }
+        }
+        else {
+            if( ((PLRU >> 6) & 0x1) == 0 ) {
+                way = 3;
+            }
+            else {
+                way = 2;
+            }
+        }
+    }
+    else {
+        if( ((PLRU >> 1) & 0x1) == 0 ) {
+            if( ((PLRU >> 5) & 0x1) == 0 ) {
+                way = 5;
+            }
+            else {
+                way = 4;
+            }
+        }
+        else {
+            if( ((PLRU >> 4) & 0x1) == 0 ) {
+                way = 7;
+            }
+            else {
+                way = 6;
+            }
+        }
+    }
+
+    return way;
+}
+
+int findEmpty(unsigned int setIndex) {
+    int way;
+
+    for (int i = 0; i < NUMWAYS; i++) {
+        if (dCache[setIndex].lines[i].MESI == 'I') {
+            return way = i;
+        }
+    }
+    return -1;
+}
+
+int findHit(Derived values) {
+    int way;
+    for (int i = 0; i < NUMWAYS; i++) {
+        if (dCache[values.index].lines[i].tag == values.tag) {
+            return way = i;
+        }
+    }
+    return -1;
+}
+
+void messageToCache(int Message, unsigned int Address) {
+    if(!silentMode) {
+        printf("L2: %s %x\n", messageChar[Message], Address);
+    }
+}
+
+void printValid(unsigned int address) {
+    Derived values;
+    values = addressParser(address);
+
+    for (int i = 0; i < NUMWAYS; i++) {
+        if (dCache[values.index].lines[i].MESI == 'I') {
+            continue;
+        }
+        else {
+            printf("\nCache Set   : %d\n",  values.index);
+            printf("Line        : %d\n",    i);
+            printf("Tag         : 0x%x\n",  dCache[values.index].lines[i].tag);
+            printf("MESI        : %c\n\n",  dCache[values.index].lines[i].MESI);
+        }
+    }
+}
+
+void printOutput() {
+    printf("\nTotal Cache Reads:    %d\n",    output.cacheReads);
+    printf("Total Cache Writes:   %d\n",      output.cacheWrites);
+    printf("Total Cache Hits:     %d\n",      output.cacheHits);
+    printf("Total Cache Misses:   %d\n",      output.cacheMiss);
+    printf("Cache Hit Ratio:      %.3f\n\n",  output.cacheRatio);
+}
+
+void resetCache() {
+    for (int i = 0; i < NUMSETS; i++) {
+        for (int j = 0; j < NUMWAYS; j++) {
+            dCache[i].lines[j].MESI = 'I';
+            dCache[i].lines[j].tag = 0;
+            iCache[i].lines[j].MESI = 'I';
+            iCache[i].lines[j].tag = 0;
+        }
+    }
+
+    output.cacheHits = 0;
+    output.cacheMiss = 0;
+    output.cacheRatio = 0;
+    output.cacheReads = 0;
+    output.cacheWrites = 0.0;
+
+}
+
+Derived addressParser(unsigned int address) {
+    Derived values;
+
+    values.index = (address >> BYTEOFF) & INDEXMASK;
+    values.tag = (address >> (BYTEOFF + INDEXOFF)) & TAGMASK;
+    return values;
+}
+
+TraceValues traceParser(char *filename) {
     FILE *fp;
-    traceValues trace;
+    TraceValues trace;
     int i = 0;
     fp = fopen(filename, "r");
 
@@ -48,17 +307,18 @@ traceValues traceParser(char *filename) {
         exit(-1);
     }
 
-    if(silentModeGbl == false) {
+    if(!silentMode) {
         printf("===Command===|===Address===\n");
     }
 
     // Reads and parses file into struct
     while(fscanf(fp, "%d %x", &trace.command[i], &trace.address[i]) != EOF) {
-        if(silentModeGbl == false) {
+        if(!silentMode) {
             printf("      %d          %x\n", trace.command[i], trace.address[i]);
         }
         i++;
     }
+    trace.size = i;
 
     return trace;
 }
--- main.h
+++ main.h
@@ -2,18 +2,139 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define BUFFER 100
+/*----------------------------------
+              Defines           
+----------------------------------*/
+#define BUFFER      100
+#define INDEXMASK   0x00007FFF
+#define TAGMASK     0x000007FF
+#define BYTEOFF     6
+#define INDEXOFF    15
+#define NUMWAYS     8
+#define NUMSETS     32768
 
 
+/*----------------------------------
+               Enums            
+----------------------------------*/
+/* Bus Operation types */
+enum BusOp {
+    READ,
+    WRITE,
+    INVALIDATE,
+    RWIM
+};
 
-// Global flag to set mode
-bool silentModeGbl;
+/* Snoop Result types */
+enum SnoopOp {
+    NOHIT,
+    HIT,
+    HITM
+};
 
-// Struct to hold values from trace
+/* L2 to L1 message types */
+enum MessageType {
+    GETLINE,
+    SENDLINE,
+    INVALIDATELINE,
+    EVICTLINE
+};
+
+enum Commands {
+    READREQ,
+    WRITEREQ,
+    READREQI,
+    SNOOPINVALD,
+    SNOOPREAD,
+    SNOOPWRITE,
+    SNOOPRWIN,
+    TEST,
+    CLR,
+    PRINT
+};
+
+/*----------------------------------
+            Char Arrays             
+----------------------------------*/
+const char *busChar[4] = {
+    "READ",
+    "WRITE",
+    "INVALIDATE",
+    "RWIM"
+};
+
+const char *snoopChar[3] = {
+    "NOHIT",
+    "HIT",
+    "HITM"
+};
+
+const char *messageChar[4] = {
+    "GETLINE",
+    "SENDLINE",
+    "INVALIDATELINE",
+    "EVICTLINE"
+};
+
+/*----------------------------------
+          Typedef Structs              
+----------------------------------*/
 typedef struct {
     unsigned int command[BUFFER];
     unsigned int address[BUFFER];
-} traceValues;
+    unsigned int size;
+} TraceValues;
 
-// Fucntion prototypes
-traceValues traceParser(char *filename);
+typedef struct {
+    unsigned int cacheReads;
+    unsigned int cacheWrites;
+    unsigned int cacheHits;
+    unsigned int cacheMiss;
+    float        cacheRatio;
+} CacheOutput;
+
+typedef struct {
+    char         MESI;
+    unsigned int tag;
+} Lines;
+
+typedef struct {
+    unsigned int PLRU;
+    Lines lines[NUMWAYS];
+} Set;
+
+typedef struct {
+    unsigned int index;
+    unsigned int tag;
+} Derived;
+
+/*----------------------------------
+              Global           
+----------------------------------*/
+bool silentMode;
+TraceValues trace;
+CacheOutput output = {0};
+Set iCache[NUMSETS];
+Set dCache[NUMSETS];
+
+/*----------------------------------
+         Fucntion prototypes             
+----------------------------------*/
+TraceValues traceParser(char *filename);
+Derived addressParser(unsigned int address);
+void updateLRU(unsigned int setIndex, int way);
+int getLRU(unsigned int setIndex);
+int findEmpty(unsigned int setIndex);
+int findHit(Derived values);
+
+void printOutput();
+void printValid(unsigned int address);
+void resetCache();
+
+void busOperation(int BusOp, unsigned int Address, int SnoopResult);
+void putSnoopResult(unsigned int Address, int SnoopResult);
+void messageToCache(int Message, unsigned int Address);
+int getSnoopResult(unsigned int Address);
+
+void read(unsigned int address);
+void write(unsigned int address);
